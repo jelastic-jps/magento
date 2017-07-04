bd81af1a96f58df20d4a9d2992b5b1c4fe84f30c
sych74 <sych74@gmail.com>
2017-07-04 03:12:55 +0300

    Update env.php

--- configs/magento/env.php
+++ configs/magento/env.php
@@ -24,28 +24,13 @@ return array (
     array (
       'default' =>
       array (
-        'host' => 'DB_MASTER',
+        'host' => 'DB',
         'dbname' => 'magento',
         'username' => 'DB_USER',
         'password' => 'DB_PASS',
         'active' => '1',
       ),
     ),
-    'slave_connection' =>
-    array (
-      'default' =>
-      array (
-        'host' => 'DB_SLAVE',
-        'dbname' => 'magento',
-        'username' => 'DB_USER',
-        'password' => 'DB_PASS',
-        'active' => '1',
-        'model' => 'mysql4',
-        'engine' => 'innodb',
-        'initStatements' => 'SET NAMES utf8;',
-      ),
-    ),
-    
   ),
   'resource' =>
   array (
