c7a4304d4a2604c2ae91621c1b9429b079c3a6d8
alexey.lazarenko <dzotic9@gmail.com>
2023-01-16 15:51:34 +0200

    JE-64765  [Magento standalone/cluster] Update Magento on PHP8.0

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -6,7 +6,7 @@ var resp = {
 if ('${settings.ls_addon:false}'== 'true') {
   resp.nodes.push({
     nodeType: "llsmp",
-    engine: "php8.0",
+    engine: "php8.1",
     flexibleCloudlets: ${settings.cp_flexibleCloudlets:32},
     fixedCloudlets: ${settings.cp_fixedCloudlets:1},
     nodeGroup: "cp",
@@ -31,7 +31,7 @@ if ('${settings.ls_addon:false}'== 'true') {
 } else {
   resp.nodes.push({
     nodeType: "lemp",
-    engine: "php8.0",
+    engine: "php8.1",
     flexibleCloudlets: ${settings.cp_flexibleCloudlets:32},                  
     fixedCloudlets: ${settings.cp_fixedCloudlets:1},
     nodeGroup: "cp",
